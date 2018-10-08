7c73037b4541d7c0a6606bd824522b219e92b8d8
kayleighskinner <kayleighanneskinner@gmail.com>
2018-10-08 17:01:58 -0500

    test test

---
+++ beforeafter.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+
+//When you click on te before image, fade that image out
+
+
+$(".before").click(function(){
+$(".before").fadeOut(1500);
+
+});
+
+$(".after").click(function(){
+	$(".before").fadeIn(1500);
+	
+});
+
+
+//When you click on the after image, fade the before image out
+  
+    
+});
